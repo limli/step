751cf82429104c5d32693a66f461e7e109b6ac3a
Lim Li <limlirandom@gmail.com>
2020-05-29 10:47:04 +0800

    Move datastore initialization to constructor

--- portfolio/src/main/java/com/google/sps/servlets/DeleteServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteServlet.java
@@ -14,13 +14,18 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/delete-data")
 public class DeleteServlet extends HttpServlet {
 
+  private final DatastoreService datastore;
+
+  public DeleteServlet() {
+    datastore = DatastoreServiceFactory.getDatastoreService();
+  }
+
   /** Deletes the comment with the given id parameter */
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     long id = Long.parseLong(request.getParameter("id"));
 
     Key taskEntityKey = KeyFactory.createKey("Comment", id);
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.delete(taskEntityKey);
   }
 }
