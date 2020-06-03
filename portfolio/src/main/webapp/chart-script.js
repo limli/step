0f8f1b2b80847214eecbf61b13192db7e572c0db
Lim Li <limlirandom@gmail.com>
2020-06-03 04:36:44 +0000

    Add dropdown to select country

--- portfolio/src/main/webapp/chart-script.js
+++ portfolio/src/main/webapp/chart-script.js
@@ -4,11 +4,15 @@ const MIN_DATE = new Date(Date.UTC(2020, 0, 22)); // 22 Jan 2020
 
 let data = {};
 let date;
+const dropdownValues = ['Singapore', 'China', 'Malaysia'];
+const dropdownIsSelected = [false, false, false];
+const dropdownCheckboxElements = [];
 
 /**
  * initializes data
  */
 function init() {
+  initDropdown();
   update();
   const countries = ['Singapore', 'China'];
   const countriesParam = countries.join();
@@ -18,6 +22,79 @@ function init() {
         data = obj;
         drawChart();
       });
+}
+
+/**
+ * Initializes dropdown
+ */
+function initDropdown() {
+  const dropdown = document.getElementById('dropdown');
+
+  const header = document.createElement('div');
+  header.innerText = 'Select Countries';
+  header.className = 'dropdown-button';
+  dropdown.appendChild(header);
+
+  const checkboxes = document.createElement('div');
+  checkboxes.className = 'dropdown-menu';
+  for (let i=0; i<dropdownValues.length; i++) {
+    const value = dropdownValues[i];
+
+    const row = document.createElement('div');
+    row.className = 'dropdown-item';
+    row.onclick = () => dropdownItemOnClick(i);
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    row.appendChild(checkbox);
+    dropdownCheckboxElements.push(checkbox);
+
+    const text = document.createElement('span');
+    text.innerText = value;
+    row.appendChild(text);
+
+    checkboxes.appendChild(row);
+  }
+  checkboxes.style.display = 'none';
+  dropdown.appendChild(checkboxes);
+
+  // show/hide dropdown menu on click
+  header.onclick = () => {
+    if (checkboxes.style.display == 'none') {
+      checkboxes.style.display = 'block';
+    } else {
+      checkboxes.style.display = 'none';
+    }
+  };
+}
+
+/**
+ * Updates state for dropdown menu
+ * @param {number} index
+ */
+function dropdownItemOnClick(index) {
+  dropdownIsSelected[index] = !dropdownIsSelected[index];
+  const value = dropdownValues[index];
+  dropdownUpdateUI();
+}
+
+/**
+ * Updates dropdown UI based on global states
+ */
+function dropdownUpdateUI() {
+  const header = document.getElementById('dropdown')
+      .querySelector('.dropdown-button');
+  const dropdownSelectedValues = [];
+  for (let i = 0; i < dropdownCheckboxElements.length; i++) {
+    dropdownCheckboxElements[i].checked = dropdownIsSelected[i];
+    if (dropdownIsSelected[i]) {
+      dropdownSelectedValues.push(dropdownValues[i]);
+    }
+  }
+
+  header.innerText = dropdownSelectedValues.length == 0 ?
+      'Select Countries' :
+      dropdownSelectedValues.join(', ');
 }
 
 /** Redraws chart based on date selected. */
