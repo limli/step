0c4faf401a020729279da0e4cffed3af5b992dbb
Lim Li <limlirandom@gmail.com>
2020-06-12 03:34:56 +0000

    add plain map

---
+++ portfolio/src/main/webapp/map-script.js
@@ -0,0 +1,10 @@
+
+/**
+ * Initializes map
+ */
+function initMap() {
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 1.276683, lng: 103.799544},
+    zoom: 15,
+  });
+}
